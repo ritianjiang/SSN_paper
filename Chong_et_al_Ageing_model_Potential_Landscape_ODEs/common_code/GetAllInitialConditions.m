3a13a4a416130eeac2822631ec81498d88ba2007
Haotian Wang <ritianjiang@gmx.com>
2025-05-26 15:42:10 +0800

    Delete Chong_et_al_Ageing_model_Potential_Landscape_ODEs directory

--- Chong_et_al_Ageing_model_Potential_Landscape_ODEs/common_code/GetAllInitialConditions.m
+++
@@ -1,8 +0,0 @@
-function res = GetAllInitialConditions(fromInitialCondition, toInitialCondition, trajectoryNumber)
-
-res = zeros(trajectoryNumber, length(fromInitialCondition));
-for j = 1:length(fromInitialCondition)
-    % Set the initial conditions randomly in the state space
-    res(:, j) = rand(trajectoryNumber, 1)*(toInitialCondition(j)-fromInitialCondition(j)) ...
-    + fromInitialCondition(j);
-end
--- Chong_et_al_Ageing_model_Potential_Landscape_ODEs/common_code/GetOneTrajectory.m
+++
@@ -1,3 +0,0 @@
-function res = GetOneTrajectory(t, initialCondition)
-% Calculate the time course simulations using MATLAB built in function ode45 
-[T res] = ode45(@equations, t,  initialCondition, []);
